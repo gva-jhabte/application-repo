ce351cf74649da7f82c348af97797c410387147f
Miya Pniel <miya.pniel@lloydsbanking.com>
2021-03-16 14:35:51 +0000

    Mitre makes table

--- transformations/MITRE_EDB_MAP/main.py
+++ transformations/MITRE_EDB_MAP/main.py
@@ -8,7 +8,8 @@ from operators import AcquireExploitDbReference
 from gva.utils.common import build_context
 from flask_sslify import SSLify
 from flask_cors import CORS
-
+import sqlalchemy
+import os
 
 from gva.logging import get_logger
 logger = get_logger()
@@ -16,7 +17,115 @@ logger.setLevel(5)
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-sslify = SSLify(app)
+# sslify = SSLify(app)
+def init_connection_engine():
+    db_config = {
+        # [START cloud_sql_postgres_sqlalchemy_limit]
+        # Pool size is the maximum number of permanent connections to keep.
+        "pool_size": 5,
+        # Temporarily exceeds the set pool_size if no connections are available.
+        "max_overflow": 2,
+        # The total number of concurrent connections for your application will be
+        # a total of pool_size and max_overflow.
+        # [END cloud_sql_postgres_sqlalchemy_limit]
+
+        # [START cloud_sql_postgres_sqlalchemy_backoff]
+        # SQLAlchemy automatically uses delays between failed connection attempts,
+        # but provides no arguments for configuration.
+        # [END cloud_sql_postgres_sqlalchemy_backoff]
+
+        # [START cloud_sql_postgres_sqlalchemy_timeout]
+        # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+        # new connection from the pool. After the specified amount of time, an
+        # exception will be thrown.
+        "pool_timeout": 30,  # 30 seconds
+        # [END cloud_sql_postgres_sqlalchemy_timeout]
+
+        # [START cloud_sql_postgres_sqlalchemy_lifetime]
+        # 'pool_recycle' is the maximum number of seconds a connection can persist.
+        # Connections that live longer than the specified amount of time will be
+        # reestablished
+        "pool_recycle": 1800,  # 30 minutes
+        # [END cloud_sql_postgres_sqlalchemy_lifetime]
+    }
+
+    if os.environ.get("DB_HOST"):
+        return init_tcp_connection_engine(db_config)
+    else:
+        return init_unix_connection_engine(db_config)
+
+
+def init_tcp_connection_engine(db_config):
+    # [START cloud_sql_postgres_sqlalchemy_create_tcp]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
+
+    # Extract host and port from db_host
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # postgres+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL(
+            drivername="postgresql+pg8000",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            host=db_hostname,  # e.g. "127.0.0.1"
+            port=db_port,  # e.g. 5432
+            database=db_name  # e.g. "my-database-name"
+        ),
+        **db_config
+    )
+    # [END cloud_sql_postgres_sqlalchemy_create_tcp]
+    pool.dialect.description_encoding = None
+    return pool
+
+
+def init_unix_connection_engine(db_config):
+    # [START cloud_sql_postgres_sqlalchemy_create_socket]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+
+        # Equivalent URL:
+        # postgres+pg8000://<db_user>:<db_pass>@/<db_name>
+        #                         ?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
+        sqlalchemy.engine.url.URL(
+            drivername="postgresql+pg8000",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_sock": "{}/{}/.s.PGSQL.5432".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        **db_config
+    )
+    # [END cloud_sql_postgres_sqlalchemy_create_socket]
+    pool.dialect.description_encoding = None
+    return pool
+
+
+# This global variable is declared with a value of `None`, instead of calling
+# `init_connection_engine()` immediately, to simplify testing. In general, it
+# is safe to initialize your database connection pool when your script starts
+# -- there is no need to wait for the first request.
+db = None
 
 def build_flow(context: dict):
 
@@ -38,6 +147,18 @@ def build_flow(context: dict):
 
     return flow
 
+@app.before_first_request
+def create_tables():
+    global db
+    db = init_connection_engine()
+    # Create tables (if they don't already exist)
+    with db.connect() as conn:
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS state "
+            "( data_feed SERIAL NOT NULL, date DATE NOT NULL, "
+            "last_state VARCHAR(255) NOT NULL );"
+        )
+
 @app.route('/ingest', methods=["POST"])
 def main(context: dict = {}):
     context['config_file'] = 'MITRE_EDB_MAP.metadata'
@@ -58,4 +179,8 @@ def main(context: dict = {}):
     return 'Finished Ingest'
 
 if __name__ == "__main__":
-    app.run(ssl_context="adhoc", host="0.0.0.0", port=8080)
+    # app.run(ssl_context="adhoc", host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
+    # context = {}
+    # context['config_file'] = 'MITRE_EDB_MAP.metadata'
+    # main(context)
